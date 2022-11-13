37388ca1af7638c52dfd7a5c9f94b1cdc00a8598
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-13 12:19:48 -0300

    fixing linter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import HandleRender from './components/HandleRender';
 import Card from './components/Card';
