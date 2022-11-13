c60ddf238bd50dc3918dc5e3655a217f112de30e
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-11-13 11:16:47 -0300

    Card estilizacao iniciada

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HandleRender from './components/HandleRender';
 import Card from './components/Card';
 import Filter from './components/Filter';
 import Form from './components/Form';
+import './style/App.scss';
 
 function App() {
   const [cardName, setCardName] = useState('');
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../style/Card.scss';
 
 function Card({
   cardName,
@@ -12,15 +13,17 @@ function Card({
   cardTrunfo,
 }) {
   return (
-    <div>
+    <div className="Card">
       <h1 data-testid="name-card">{cardName}</h1>
-      <img data-testid="image-card" src={ cardImage } alt={ cardName } />
-      <p data-testid="description-card">{cardDescription}</p>
-      <p data-testid="attr1-card">{cardAttr1}</p>
-      <p data-testid="attr2-card">{cardAttr2}</p>
-      <p data-testid="attr3-card">{cardAttr3}</p>
-      <p data-testid="rare-card">{cardRare}</p>
-      {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
+      <div>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        {cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p>}
+      </div>
     </div>
   );
 }
